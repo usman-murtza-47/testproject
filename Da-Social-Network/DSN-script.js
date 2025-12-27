233d07fc6488ae9096ddd7e3ea4d56866076bd8f
usman-murtza-47 <usmanmurtza11@gmail.com>
2025-12-27 15:00:48 +0530

    Add files via upload

---
+++ Da-Social-Network/DSN-script.js
@@ -0,0 +1,68 @@
+// HERE ARE ALL THE VARIABLES WE ARE FOING TO USE IN THE PROGRAM
+
+// 1.NAVBAR VARIABLES
+const headerOne = document.getElementById("header1");
+const headerTwo = document.getElementById("header2");
+const postButton = document.getElementById("postbtn");
+const backButton = document.getElementById("backbtn");
+
+// 2.HERO-SECTION VARIABLES
+const pageOne = document.getElementById("section1");
+const pageTwo = document.getElementById("section2");
+const sharePostButton = document.getElementById("sharepostbtn");
+
+
+// HERE ARE THE DEFAULT CONDITIONS OF THE PAGE WHEN THE PAGE IS FIRST OPENED BY THE USER
+headerOne.style.display = "flex";
+headerTwo.style.display = "none";
+pageOne.style.display = "flex";
+pageTwo.style.display = "none";
+
+// Here is what happens if a user clicks on any button:
+
+// 1.Post Button
+postButton.addEventListener('click', function () {
+  headerOne.style.display = "none";
+  headerTwo.style.display = "flex";
+  pageOne.style.display = "none";
+  pageTwo.style.display = "flex";
+});
+
+// 2.BACK BUTTON
+backButton.addEventListener('click', function () {
+  headerOne.style.display = "flex";
+  headerTwo.style.display = "none";
+  pageOne.style.display = "flex";
+  pageTwo.style.display = "none";
+})
+
+// 3.Share Post Button
+sharePostButton.addEventListener('click', function () {
+  let userInput = document.getElementById("postmsg").value;
+  if (userInput === "") {
+    console.log("Write a valid message");
+    const msgString=  `<p>Message cannot be empty</p>`;
+    document.getElementById("postmsg").insertAdjacentHTML('afterend',msgString);
+  }
+  else {
+    const htmlstring = `      <div class="message">
+          <h5 class="username">StickMan-47</h5>
+          <p class="msginfo">${userInput}</p>
+          <div class="interaction">
+            <button class="likebtn"><img src="Images/heart-outline.svg" class="btnimg" alt="">Like</button>
+            <button class="commentbtn"> <img src="Images/chatbubble-outline.svg" class="btnimg" alt="">Comment</button>
+            <button class="sharebtn"><img src="Images/share-social-outline.svg" class="btnimg" alt=""> Share</button>
+          </div>
+        </div>`
+
+    pageOne.insertAdjacentHTML('afterbegin', htmlstring);
+
+
+    headerOne.style.display = "flex";
+    headerTwo.style.display = "none";
+    pageOne.style.display = "flex";
+    pageTwo.style.display = "none";
+
+    document.getElementById("postmsg").value = "";
+  }
+})
