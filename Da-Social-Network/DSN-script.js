a94e315bc38f3abb995c7174dd410c5688760a6f
usman-murtza-47 <usmanmurtza11@gmail.com>
2025-12-29 20:27:33 +0530

    Update DSN-script.js

--- Da-Social-Network/DSN-script.js
+++ Da-Social-Network/DSN-script.js
@@ -11,15 +11,23 @@ const UserButton=document.getElementById("userid");
 const pageOne = document.getElementById("section1");
 const pageTwo = document.getElementById("section2");
 const pageThree = document.getElementById("section3");
+const pageFour=document.getElementById("section4");
+const pageFive=document.getElementById("section5");
+const pageSix=document.getElementById("section6");
 const sharePostButton = document.getElementById("sharepostbtn");
+const nameChangeButton=document.getElementById("namebtn");
 
 
 // HERE ARE THE DEFAULT CONDITIONS OF THE PAGE WHEN THE PAGE IS FIRST OPENED BY THE USER
-headerOne.style.display = "flex";
-headerTwo.style.display = "none";
-pageOne.style.display = "flex";
+headerOne.style.display = "none";
+headerTwo.style.display = "flex";
+pageOne.style.display = "none";
 pageTwo.style.display = "none";
-pageThree.style.display="none";
+pageThree.style.display="flex";
+pageFour.style.display="none";
+pageFive.style.display="none";
+pageSix.style.display="none";
+
 
 // Here is what happens if a user clicks on any button:
 
@@ -50,15 +58,16 @@ sharePostButton.addEventListener('click', function () {
     document.getElementById("postmsg").insertAdjacentHTML('afterend',msgString);
   }
   else {
-    const htmlstring = `      <div class="message">
-          <h5 class="username">StickMan-47</h5>
-          <p class="msginfo">${userInput}</p>
-          <div class="interaction">
-            <button class="likebtn"><img src="Images/heart-outline.svg" class="btnimg" alt="">Like</button>
-            <button class="commentbtn"> <img src="Images/chatbubble-outline.svg" class="btnimg" alt="">Comment</button>
-            <button class="sharebtn"><img src="Images/share-social-outline.svg" class="btnimg" alt=""> Share</button>
-          </div>
-        </div>`
+    const htmlstring = `
+          <div class="message">
+        <p class="username">StickMan-47</p>
+        <p class="msginfo">${userInput}</p>
+        <div class="interaction">
+          <button class="likebtn"><img src="Images/heart-outline.svg" class="btnimg" alt="">Like</button>
+          <button class="commentbtn"> <img src="Images/chatbubble-outline.svg" class="btnimg" alt="">Comment</button>
+          <button class="sharebtn"><img src="Images/share-social-outline.svg" class="btnimg" alt=""> Share</button>
+        </div>
+      </div>`
 
     pageOne.insertAdjacentHTML('afterbegin', htmlstring);
 
@@ -80,4 +89,9 @@ UserButton.addEventListener('click',function(){
     pageOne.style.display = "none";
     pageTwo.style.display = "none";
     pageThree.style.display="flex";
+});
+// 5.nameChangeButton
+nameChangeButton.addEventListener('click',function(){
+  pageThree.style.display="none";
+  pageFour.style.display="flex";
 })
